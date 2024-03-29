e5fb7664a1e3429934d18abd2591d754a0143589
sin1111yi <sin1111yi@foxmail.com>
2024-03-30 03:56:00 +0800

    add rofi theme and wallpaper script

---
+++ hypr/scripts/wallpaper-set.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+h_wallpaper_path=$HOME/.wallpapers/h
+v_wallpaper_path=$HOME/.wallpapers/v
+
+random_h_wallpaper=$(find "$h_wallpaper_path" -type f | shuf -n 1)
+random_v_wallpaper=$(find "$v_wallpaper_path" -type f | shuf -n 1)
+
+swww img "$random_h_wallpaper" -o DP-1 --resize fit
+swww img "$random_v_wallpaper" -o DP-3 --resize fit
